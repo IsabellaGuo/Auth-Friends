0485d8f921dadda6b297ad95519b43ba0f4f332c
IsabellaGuo <isabella.guofei@gmail.com>
2020-01-28 21:52:40 -0800

    Created friends card

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,4 +1,4 @@
 import React, { useState, useEffect } from "react";
 import Loader from "react-loader-spinner";
 import axios from "axios";
-import { axiosWithAuth } from ""
+import { axiosWithAuth } from "../utils/axiosWithAuth";

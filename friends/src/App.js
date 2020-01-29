c36cca6094b286fe9731ff72f90891b4911198f6
IsabellaGuo <isabella.guofei@gmail.com>
2020-01-29 11:23:44 -0800

    Updated PrivateRoute

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { Route, Link, Switch, NavLink } from 'react-router-dom';
 
 import './App.css';
+
+import Login from './components/Login';
+
+import Friends from './components/Friend';
+import PrivateRoute from './components/PrivateRoute.js';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <div>
+        <nav className="nav">
+          <NavLink to="/api/friends"> My Friends (Protected) </NavLink>
+          <NavLink to="/api/login"> Login </NavLink>
+        </nav>
+        
+      </div>
+      <section className="Routes">
+      <Switch>
+        
+        <PrivateRoute exact path="/api/friends" component={Friends} />
+        <Route exact path="/api/login" component={Login} />
+        <Route path="/" component={Login} />
+      </Switch>
+      </section>
+
     </div>
+
   );
 }
 
--- friends/src/components/Friends.js
+++
@@ -1,4 +0,0 @@
-import React, { useState, useEffect } from "react";
-import Loader from "react-loader-spinner";
-import axios from "axios";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
